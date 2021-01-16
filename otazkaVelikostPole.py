b9b60dad5224dcc34660bad1402f666ad607985f
JenikNo4 <JenikNo.4@centrum.cz>
2021-01-16 17:37:01 +0100

    with menu

--- otazkaVelikostPole.py
+++ otazkaVelikostPole.py
@@ -1,16 +1,18 @@
 from pygame_functions import *
 
+
 def velikostPole(maxWidth, maxHeigth):
     # zeptá se uzivatele na velikost minového pole
     # vrací šířku a výsku pole
     # typ vracených hodnot je iteger
 
-    screenSize(500,300)
+    global heigth
+    screenSize(maxWidth, maxHeigth)
 
-    otazka = makeLabel(" Zadejte velikost minového pole ",40,10,10, "blue","","clear")
+    otazka = makeLabel(" Zadejte velikost minového pole ", 40, 10, 10, "blue", "", "clear")
     showLabel(otazka)
 
-    wordBox1 = makeTextBox(30,80,200,0," šířka (max "+ str(maxWidth) +")",3,24)
+    wordBox1 = makeTextBox(30, 80, 200, 0, " šířka (max " + str(maxWidth) + ")", 3, 24)
     showTextBox((wordBox1))
 
     # uživatelský vstup - šířka
@@ -24,14 +26,15 @@ def velikostPole(maxWidth, maxHeigth):
                 chyba = True
             else:
                 chyba = False
-        except: ValueError
+        except:
+            ValueError
 
     # zobrazí zadanou šířku
-    labelWidth = makeLabel(width,40,40,85,"blue","","clear")
+    labelWidth = makeLabel(width, 40, 40, 85, "blue", "", "clear")
     showLabel(labelWidth)
 
     # uživatelský vstup - výška
-    wordBox2 = makeTextBox(30,150,200, 0, " výška (max " + str(maxHeigth) + ")", 3, 24)
+    wordBox2 = makeTextBox(30, 150, 200, 0, " výška (max " + str(maxHeigth) + ")", 3, 24)
 
     # cyklus dokud nezada uživatel číslo > 0 a menší než max
     chyba = True
@@ -45,13 +48,13 @@ def velikostPole(maxWidth, maxHeigth):
                 chyba = True
             else:
                 chyba = False
-        except: ValueError
+        except:
+            ValueError
 
-
-    return(int(width), int(heigth))
+    return (int(width), int(heigth))
 
 # zavolám dialog velikosti minového pole a vytisku co uživatel zadal
 # vstupní parametry jsou maximální rozměry minového pole
 # 1. - max šířka minového pole
 # 2. - max výška minového pole
-print(velikostPole(800,600))
+# print(velikostPole(800,600))
